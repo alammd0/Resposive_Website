7b5300ff253c7f68dbc2824f42e1860bb7a53bd9
Md Khalid Alam <sonualam96314@gmail.com>
2024-12-22 16:41:17 +0530

    complete navBar

--- EdtechClone/App.js
+++ EdtechClone/App.js
@@ -7,3 +7,12 @@ menuLinks.forEach((link) => {
         link.classList.add('active');
     });
 });
+
+
+// add toggle system 
+const menuToggle = document.getElementById("menu_toggle");
+const navMenu = document.querySelector(".nav_menu"); 
+
+menuToggle.addEventListener("click", () => {
+    navMenu.classList.toggle("show");
+})
